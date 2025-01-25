97f8fba290c645d78cfaf29885b892a9cd786f97
germgallardo <german.mgallardo@gmail.com>
2025-01-25 22:22:17 +0100

    adding pytest.mark.parametrize decorator to run the function with multiple test cases

--- test_time_format.py
+++ test_time_format.py
@@ -1,27 +1,24 @@
+# This file contains test cases for the `time_format` function to check different time duration formats to ensure they return the expected output
+# For running the tests: pytest -v test_time_format.py
+# The `-v` flag (verbose) is used for providing more details for each of the test cases during the execution
+
+from test import time_format
 import pytest
 
 
-def time_format(duration):
-	'''
-	YT uses ISO 8601 to format time and return it as a string as PT1H4M3S (PT is Time Duration, H is Hour, M is Minute and S is Second)
-	This function dissects the string and separates hours, minutes and seconds for a more convenient view
-	'''
-	try:
-		duration = duration[2:]
-		#print(duration)
-		h, m, s = duration.find("H"), duration.find("M"), duration.find("S")
-		seconds = duration[m+1:s]
-		if h > 0:
-			minutes = duration[h+1:m]
-			hours = duration[:h]
-			return hours + " hours " + minutes + " minutes " + seconds + " seconds"
-		else:
-			minutes = duration[:m]
-			return minutes + " minutes " + seconds + " seconds"
+# The pytest.mark.parametrize decorator allows running the same `test_time_format` function with multiple test cases
+# Each tuple contains the input (`duration`) for the `time_format` function and the expected output (`expected_output`)
+# By using this decorator, the function will run consecutively according to the number of tuples that we define, in this case 6
+# For more information: https://docs.pytest.org/en/stable/how-to/parametrize.html
+@pytest.mark.parametrize("duration, expected_output", [
+    ("PT1H4M3S", "1 hours 4 minutes 3 seconds"),
+    ("PT11H50M20S", "11 hours 50 minutes 20 seconds"),
+    ("PT07H04M02S", "07 hours 04 minutes 02 seconds"),
+    ("PT7M23S", "7 minutes 23 seconds"),
+    ("PT09M07S", "09 minutes 07 seconds"),
+    ("PT27H03M11S", "27 hours 03 minutes 11 seconds")
+])
 
-	except Exception as e:
-		print(f"Time/Hour format error: {e}")
-
-
-def test_time_format():
-	assert time_format("PT1H4M3S") == "1 hours 4 minutes 3 seconds"
+def test_time_format(duration, expected_output):
+    # with `assert` we can check if `duration` matches with the `expected_output`, if not it will raise an error and the test will fail
+    assert time_format(duration) == expected_output
